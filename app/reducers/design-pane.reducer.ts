d8cbd9c5c4a0ee8950a6aa22db857c6eb19dffa1
HazelDaniel <hazeldaniel856@gmail.com>
2024-10-14 11:35:57 +0100

    added the design pane reducer

---
+++ app/reducers/design-pane.reducer.ts
@@ -0,0 +1,51 @@
+export const designPaneActionTypes = {
+  setActiveTab: "SET_ACTIVE_TAB",
+};
+
+export interface DesignPaneActionType<T> {
+  type: keyof typeof designPaneActionTypes;
+  payload: T;
+}
+
+export interface DesignPaneStateType {
+  activeTab: "comment" | "table" | "text" | null;
+}
+
+export const initialDesignPaneState: DesignPaneStateType = {
+  activeTab: null,
+};
+
+export const designPaneReducer: (
+  state: DesignPaneStateType,
+  action: DesignPaneActionType<unknown>
+) => DesignPaneStateType = (state = initialDesignPaneState, action) => {
+  let newState: DesignPaneStateType;
+  switch (action.type) {
+    case "setActiveTab": {
+      if (state.activeTab === action.payload) return state;
+      newState = { ...state };
+      newState.activeTab = action.payload as
+        | "comment"
+        | "table"
+        | "text"
+        | null;
+      return newState;
+    }
+    default:
+      return state;
+  }
+};
+
+
+// ACTION PRODUCERS
+
+export const __setActiveTab: (
+  tab: "comment" | "table" | "text" | null
+) => DesignPaneActionType<"comment" | "table" | "text" | null> = (tab) => {
+  return {
+    type: "setActiveTab",
+    payload: tab,
+  };
+};
+
+// SELECTORS
