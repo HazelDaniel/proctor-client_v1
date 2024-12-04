7de7db9de0dfedd37cde13ef3c024d914c353632
HazelDaniel <hazeldaniel856@gmail.com>
2024-12-04 17:14:34 +0100

    added the composition reducer

---
+++ app/reducers/composition.reducer.ts
@@ -0,0 +1,72 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { NodeCompositeID } from "~/types";
+import { parseNodeID } from "~/utils/node.utils";
+
+interface CompositionStateType {
+  [key: string]: { [key: NodeCompositeID]: { [key: string]: number } };
+}
+
+const initialCompositionState: CompositionStateType = {};
+
+export const compositionSlice = createSlice({
+  name: "tables:composition",
+  initialState: initialCompositionState,
+  reducers: {
+    addComposition: (
+      state,
+      action: PayloadAction<[NodeCompositeID, string]>
+    ) => {
+      const [nodeID, compositeColumnID] = action.payload;
+      const [parentID, _2] = parseNodeID(nodeID);
+      if (!state[parentID])
+      state[parentID] = { [nodeID]: { [compositeColumnID]: 1 } };
+      else
+      state[parentID] = {...state[parentID], [nodeID]: { ...state[parentID][nodeID],[compositeColumnID]: 1 } };
+    },
+    addCompositions: (
+      state,
+      action: PayloadAction<[NodeCompositeID, string[]]>
+    ) => {
+      const [nodeID, compositeColumnIDs] = action.payload;
+      const [parentID, _2] = parseNodeID(nodeID);
+      for (const compositeColumnID of compositeColumnIDs) {
+        if (!state[parentID])
+        state[parentID] = { [nodeID]: { [compositeColumnID]: 1 } };
+        else
+        state[parentID] = {...state[parentID], [nodeID]: { ...state[parentID][nodeID],[compositeColumnID]: 1 }}
+      }
+    },
+    removeCompositionParent: (
+      state,
+      action: PayloadAction<NodeCompositeID>
+    ) => {
+      const nodeID = action.payload;
+      const [parentID, _2] = parseNodeID(nodeID);
+      if (state[parentID] && nodeID in state[parentID])
+        delete state[parentID][nodeID];
+    },
+    removeComposition: (
+      state,
+      action: PayloadAction<[NodeCompositeID, string]>
+    ) => {
+      const [nodeID, compositeColumnID] = action.payload;
+      const [parentID, _2] = parseNodeID(action.payload[0]);
+      if (
+        state[parentID] &&
+        nodeID in state[parentID] &&
+        compositeColumnID in state[parentID][nodeID]
+      )
+        delete state[parentID][nodeID][compositeColumnID];
+    },
+  },
+});
+
+export const {
+  addComposition,
+  removeComposition,
+  addCompositions,
+  removeCompositionParent,
+} = compositionSlice.actions;
+
+export const compositionReducer = compositionSlice.reducer;
+export default compositionReducer;
