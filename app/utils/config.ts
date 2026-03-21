6f85177e3ce897fc73c7036643061a9074a2b498
HazelDaniel <hazeldaniel856@gmail.com>
2026-03-21 10:52:07 +0100

    fix: typo in environment configuration

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,3 +1,6 @@
 export const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT || 'development';
-export const API_BASE_URL = ENVIRONMENT === 'production' ? import.meta.env.VITE_BASE_URL_PROD : import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+export const API_BASE_URL = ENVIRONMENT === 'production' ? import.meta.env.VITE_API_BASE_URL_PROD : import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 export const GQL_URL = `${API_BASE_URL}/graphql`;
+
+console.log("environment is ", ENVIRONMENT);
+console.log("API BASE URL is: ", API_BASE_URL);
