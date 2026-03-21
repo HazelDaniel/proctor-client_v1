a5524727fa1f9b13c26de736708b43e472992c35
HazelDaniel <hazeldaniel856@gmail.com>
2026-03-21 13:13:01 +0100

    chore: remove console logs

--- app/utils/config.ts
+++ app/utils/config.ts
@@ -1,6 +1,3 @@
 export const ENVIRONMENT = import.meta.env.VITE_ENVIRONMENT || 'development';
 export const API_BASE_URL = ENVIRONMENT === 'production' ? import.meta.env.VITE_API_BASE_URL_PROD : import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 export const GQL_URL = `${API_BASE_URL}/graphql`;
-
-console.log("environment is ", ENVIRONMENT);
-console.log("API BASE URL is: ", API_BASE_URL);
